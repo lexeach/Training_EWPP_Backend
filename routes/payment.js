325ac380469b5a06557ab8d672013ddd8848f422
lexeach <80155340+lexeach@users.noreply.github.com>
2026-06-29 17:48:18 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 const handlebars = require('handlebars');
 const htmlPdf = require('html-pdf-node');
 
-const User = mongoose.model('User');
+//const User = mongoose.model('User');
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
